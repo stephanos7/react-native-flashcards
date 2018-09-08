88cdd90a66e664a6e4de097fa2588f717f7d7cfb
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-09-08 11:36:56 +0100

    add shell Card component

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import styles from "./styles";
+import Card from "./components/Card";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Card/>
       </View>
     );
   }
---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from "../styles";
+
+export default class Card extends React.Component {
+  render() {
+    return (
+        <Text>Una Carta</Text>
+    );
+  }
+}
