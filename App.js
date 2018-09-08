03dfc7e3341fff4db51d0710a1d68c094a35c055
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-09-08 11:34:37 +0100

    initialise repo and add stylesheet export

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from "./styles";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
