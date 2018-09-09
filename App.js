1c14914a96f92ecb321f8240b8dad8d733aa259a
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-09-09 11:47:38 +0100

    pass specific head of array card to button component

--- App.js
+++ App.js
@@ -29,17 +29,19 @@ export default class App extends React.Component {
   _renderItem = (item) => <Card animateSucces={this.animateSucces} {...item}/>
 
   render() {
+    const copyOfData = [...this.state.data];
+    const headCard = copyOfData.shift();
     return (
       <View style={styles.container}>
         <FlatList
             style={{padding:30}}
-            data={this.state.data}
+            data={this.state.data.reverse()}
             renderItem={this._renderItem}
             keyExtractor={(item,index)=> index.toString()}
             />
         <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between", margin:30}}>
           <ActionButton type="incorrect" />
-          <ActionButton type="correct" data={this.state.data} attemptItem={this.attemptItem} />
+          <ActionButton type="correct" data={headCard} attemptItem={this.attemptItem} />
         </View>
       </View>
     );
